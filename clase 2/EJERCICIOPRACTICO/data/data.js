4582d37b12ca524ec548d42ad6861ae1b3527ce1
Sebastian Martinez ramos <anamariamegia402@gmail.com>
2024-04-09 09:25:15 -0500

    ejerciciopractico

---
+++ clase 2/EJERCICIOPRACTICO/data/data.js
@@ -0,0 +1,22 @@
+export const pinturas = [
+    {
+        id:1,
+        name:"la cara",
+        image:"./IMAG/imagen 1.jpg"
+
+        },
+
+        {
+        
+        id:2,
+        name:"la cara",
+        image:"./IMAG/imagen 1.jpg"
+
+        }
+]
+
+
+
+
+
+ 
